a6d023f5d90643d2d2765b243a7196b13f0c3e84
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-10-29 14:02:40 -0400

    Timeline

--- about.php
+++ about.php
@@ -4,19 +4,60 @@ include 'top.php';
 
 <section class="about-sect">
 
-<div id="est-section">
-	<h2>Est.</h2>
-	<h1>2019</h1>
-</div>
+	<div id="about-info-container">
 
-<div id="dano-div">
-	<img src="images/dane.png" alt="Dane Pic" id="dano"/>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet nibh praesent tristique magna. Enim sed faucibus turpis in eu mi bibendum. Felis donec et odio pellentesque diam volutpat commodo sed. Risus ultricies tristique nulla aliquet enim. Tellus mauris a diam maecenas sed. Vitae justo eget magna fermentum iaculis. Malesuada pellentesque elit eget gravida cum sociis natoque. Nibh venenatis cras sed felis eget velit. Id diam maecenas ultricies mi. Et netus et malesuada fames ac turpis egestas sed. Ac odio tempor orci dapibus ultrices. Leo a diam sollicitudin tempor id eu. At urna condimentum mattis pellentesque id nibh tortor. Enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit. Sagittis eu volutpat odio facilisis mauris sit.</p><br>
+		<div class="about-info-card">
+			<h2>Est.</h2>
+			<h1>2019</h1>
+		</div>
+		<div class="about-info-card">
+			<h2>15+</h2>
+			<h1>Combined Years of Experience</h1>
+		</div>
+	</div>
 
-	<p>Nisi lacus sed viverra tellus in. Vitae et leo duis ut diam quam. Tempor orci dapibus ultrices in iaculis nunc sed augue lacus. Eget sit amet tellus cras adipiscing. A scelerisque purus semper eget. Consequat id porta nibh venenatis cras. Adipiscing at in tellus integer feugiat scelerisque varius morbi. Sit amet facilisis magna etiam tempor orci eu. In metus vulputate eu scelerisque felis. Nunc consequat interdum varius sit. Velit aliquet sagittis id consectetur purus. Morbi non arcu risus quis varius quam quisque.</p><br>
-
-	<p>Facilisis gravida neque convallis a cras. Sed vulputate mi sit amet mauris commodo. Eget aliquet nibh praesent tristique magna sit amet purus gravida. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Pretium fusce id velit ut tortor pretium viverra suspendisse potenti. A cras semper auctor neque vitae tempus quam pellentesque nec. Sodales ut eu sem integer vitae justo. Amet volutpat consequat mauris nunc. Fames ac turpis egestas sed tempus urna et. Diam phasellus vestibulum lorem sed. Feugiat pretium nibh ipsum consequat nisl. Sit amet nulla facilisi morbi tempus iaculis. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Consequat nisl vel pretium lectus quam id leo in. Massa enim nec dui nunc. Risus ultricies tristique nulla aliquet enim tortor at auctor. Ultrices neque ornare aenean euismod elementum nisi.</p>
-</div>
+	<h1 id="field-exp">Field Experience</h1>
+	<section class="timeline">
+		<div class="timeline-container timeline-left">
+			<div class="timeline-event">
+				<p>Became Groundscrew Member</p>
+				<p>The Crane Estate, Ipswich, MA</p>
+				<p>Spring 2012</p>
+			</div>
+		</div>
+		<div class="timeline-container timeline-right">
+			<div class="timeline-event">
+				<p>Promoted to Landscape Supervisor</p>
+				<p>The Crane Estate, Ipswich, MA</p>
+				<p>Spring 2015</p>
+			</div>
+		</div>
+		<div class="timeline-container timeline-left">
+			<div class="timeline-event">
+				<p>Started work as an Arborist</p>
+				<p>Ipswich, MA</p>
+				<p>Fall 2017</p>
+			</div>
+		</div>
+		<div class="timeline-container timeline-right">
+			<div class="timeline-event">
+				<p>Founded Viking Hill Landscaping</p>
+				<p>Ipswich, MA</p>
+				<p>2019</p>
+			</div>
+		</div>
+		<div class="timeline-container timeline-left">
+			<div class="timeline-event">
+				<p>Geese Extractionist</p>
+				<p>Ipswich, MA</p>
+				<p>Spring 2020</p><br>
+				<ul>
+					<li>Solved a rampant geese problem plaguing a local neighborhood</li>
+					<li>Dane Tolstrup proved he is willing to do whatever it takes with no regard for the law in order to get the job done</li>
+				</ul>
+			</div>
+		</div>
+	</section>
 </section>
 <?php
 include 'footer.php';
