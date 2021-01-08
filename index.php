ab5b7af6b5b024e5f930665bf342ad7d6b3b5ae4
Robert WarrenIII <robert.warren-iii@uvm.edu>
2021-01-08 10:57:29 -0500

    Changed mantra

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ include 'top.php';
 ?>
 
 <section class="showcase">
-	<h1>Good Guys. Local Guys.</h1>
+	<h1>40 Years Combined Experience Serving North Shore Massachusetts</h1>
 </section>
 
-<section id="location">
+<!-- <section id="location">
 	<div id="location-text-div"><h1>Providing Services In and Around Ipswich, Massachusetts</h1></div>
-<!-- 	<div id="location-div"><iframe width="600" height="375" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Ipswich,%20MA+(Viking%20Hill%20Landscaping)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.maps.ie/route-planner.htm"></a></div> -->
-</section>
+	<div id="location-div"><iframe width="600" height="375" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Ipswich,%20MA+(Viking%20Hill%20Landscaping)&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.maps.ie/route-planner.htm"></a></div>
+</section> -->
 
 <div id="services-section">
 	<h1>Our Services</h1>
